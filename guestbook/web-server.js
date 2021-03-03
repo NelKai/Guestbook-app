903a622e423917a2abf20fde467e1e824c1dbdca
NelKai <55228995+NelKai@users.noreply.github.com>
2021-03-03 19:32:10 +0200

    Projektitiedostojen päivitys

--- guestbook/web-server.js
+++ guestbook/web-server.js
@@ -9,17 +9,73 @@ http.createServer(function (request, response) {
 var express = require("express");
 var app = express();
 
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+//console.log(data);
+
+//app.use(express.static("./guestbookSite"));
+
+var messages = [];
+
 
 app.get("/", function(request, response) {
-    response.sendFile(__dirname + "\\etusivu.html");
+    response.sendFile(__dirname + "\\index.html");
 });
 
 app.get("/guestbook", function(request, response) {
-    response.sendFile(__dirname + "\\viestit.html");
+    //response.sendFile(__dirname + "\\viestit.html");
+    var data = require("./guestbook-data.json");
+    var output = "<table border=2px cellpadding='15' style='border-collapse:collapse;'><tr><th>Käyttäjänimi</th><th>Maa</th><th>Viesti</th></tr>";
+
+    for (var i = 0; i < data.length; i++) {
+        output += "<tr><td>" + data[i].username + "</td><td>" + data[i].country + "</td><td>" + data[i].message + "</td></tr>";
+    }
+    output += "</table>";
+
+    response.send(output);
+    
 });
 
 app.get("/newmessage", function(request, response) {
-    response.send("NEW MESSAGE");
+    response.sendFile(__dirname + "\\newmessage.html");
+});
+
+app.post("/mymessage", function(request, response) {
+    var username = request.body.username;
+    var message = request.body.message;
+
+    /*
+    var newMessage = {
+        username: request.body.username,
+        content: request.body.message,
+        date: new Date()
+    };
+    messages.push(newMessage);
+    console.log(messages[0]);
+    */
+
+    var fs = require("fs");
+
+    var guestbookdata = require("./guestbook-data.json");
+
+    var newMessage = {
+        username: request.body.username,
+        country: request.body.country,
+        date: new Date(),
+        message: request.body.message
+    };
+
+    guestbookdata.push(newMessage);
+
+    fs.writeFile("./guestbook-data.json", JSON.stringify(guestbookdata), function(err) {
+        if (err) return console.log(err);
+    });
+
+    //console.log(request.body);
+    response.send("Viestisi '" + message + "' on lähetetty " + username + "!");
 });
 
 app.get("/ajaxmessage", function(request, response) {
